8f3e007680b5b48b0dd283ebe5d74b51d672fcab
KAD128 <92460477+KAD128@users.noreply.github.com>
2021-11-04 15:39:58 +0100

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,8 @@
 #include "ui_mainwindow.h"
 #include <QtGui>
 #include <QtCore>
+#include <QSizePolicy>
+#include <QPixmap>
 
 double wartosc;
 double tolerance;
@@ -22,31 +24,11 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    wartosc = 10;
+    QPixmap pix (":/images/4paski.png");
+    int w = ui->obraz->width();
+    int h = ui->obraz->height();
+    ui->obraz->setPixmap(pix.scaled(w,h,Qt::KeepAspectRatio));
 
-/*
-        ui->pasek1->addItem("Brak"); ui->pasek1->addItem(QString::number(0) + " Czarny"); ui->pasek1->addItem(QString::number(1) + " Brązowy");
-        ui->pasek1->addItem(QString::number(2) + " Czerwony"); ui->pasek1->addItem(QString::number(3) + " Pomarańcz"); ui->pasek1->addItem(QString::number(4) + " Zółty");
-        ui->pasek1->addItem(QString::number(5) + " Zielony");  ui->pasek1->addItem(QString::number(6) + " Niebieski"); ui->pasek1->addItem(QString::number(7) + " Fioletowy");
-        ui->pasek1->addItem(QString::number(8) + " Szary");  ui->pasek1->addItem(QString::number(9) + " Biały"); ui->pasek1->currentText();
-
-
-        ui->pasek2->addItem("  "); ui->pasek2->addItem(QString::number(0) + " Czarny"); ui->pasek2->addItem(QString::number(2) + " Brązowy");
-        ui->pasek2->addItem(QString::number(3) + " Czerwony"); ui->pasek2->addItem(QString::number(4) + " Pomarańcz"); ui->pasek2->addItem(QString::number(5) + " Zółty");
-        ui->pasek2->addItem(QString::number(6) + " Zielony");  ui->pasek2->addItem(QString::number(7) + " Niebieski"); ui->pasek2->addItem(QString::number(8) + " Fioletowy");
-        ui->pasek2->addItem(QString::number(9) + " Szary");  ui->pasek2->addItem(QString::number(10) + " Biały"); ui->pasek2->currentText();
-
-        ui->pasek3->addItem(QString::number(0)); ui->pasek3->addItem(QString::number(1) + " Czarny"); ui->pasek3->addItem(QString::number(2) + " Brązowy");
-        ui->pasek3->addItem(QString::number(3) + " Czerwony"); ui->pasek3->addItem(QString::number(4) + " Pomarańcz"); ui->pasek3->addItem(QString::number(5) + " Zółty");
-        ui->pasek3->addItem(QString::number(6) + " Zielony");  ui->pasek3->addItem(QString::number(7) + " Niebieski"); ui->pasek3->addItem(QString::number(8) + " Fioletowy");
-        ui->pasek3->addItem(QString::number(9) + " Szary");  ui->pasek3->addItem(QString::number(10) + " Biały"); ui->pasek3->currentText();
-
-        ui->pasek4->addItem(QString::number(0)); ui->pasek4->addItem(QString::number(1) + " Czarny"); ui->pasek4->addItem(QString::number(2) + " Brązowy");
-        ui->pasek4->addItem(QString::number(3) + " Czerwony"); ui->pasek4->addItem(QString::number(4) + " Pomarańcz"); ui->pasek4->addItem(QString::number(5) + " Zółty");
-        ui->pasek4->addItem(QString::number(6) + " Zielony");  ui->pasek4->addItem(QString::number(7) + " Niebieski"); ui->pasek4->addItem(QString::number(8) + " Fioletowy");
-        ui->pasek4->addItem(QString::number(9) + " Szary");  ui->pasek4->addItem(QString::number(10) + " Biały"); ui->pasek4->addItem(QString::number(11) + " Złoty");
-        ui->pasek4->addItem(QString::number(12) + " Srebrny"); ui->pasek4->currentText();
-*/
 
 }
 
@@ -164,4 +146,3 @@ void MainWindow::on_pushButton_clicked()
     }
 
 }
-
--- mainwindow.h
+++ mainwindow.h
@@ -21,6 +21,10 @@ private slots:
 
     void on_label_4_linkHovered(const QString &link);
 
+    void on_graphicsView_rubberBandChanged(const QRect &viewportRect, const QPointF &fromScenePoint, const QPointF &toScenePoint);
+
+    void on_obraz_linkActivated(const QString &link);
+
 private:
     Ui::MainWindow *ui;
 };
